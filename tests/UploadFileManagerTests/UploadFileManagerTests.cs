8e4c2029b6f8aaba581f2a9a6e4937894dfa978f
Conrad Akunga <conradakunga@gmail.com>
2025-04-22 19:44:00 +0300

    Add tests for constructor

--- tests/UploadFileManagerTests/UploadFileManagerTests.cs
+++ tests/UploadFileManagerTests/UploadFileManagerTests.cs
@@ -7,6 +7,45 @@ namespace UploadFileManagerTests;
 
 public class UploadFileManagerTests
 {
+    [Fact]
+    public void UploadFile_Throws_Exception_With_Null_Compressor()
+    {
+        var encryptor = new Mock<IFileEncryptor>(MockBehavior.Strict);
+        var persistor = new Mock<IFilePersistor>(MockBehavior.Strict);
+        IFileCompressor compressor = null;
+        var fakeTimeProvider = new FakeTimeProvider();
+        fakeTimeProvider.SetUtcNow(new DateTimeOffset(2025, 1, 1, 0, 0, 0, TimeSpan.Zero));
+        var ex = Record.Exception(() =>
+            new UploadFileManager(persistor.Object, encryptor.Object, compressor, fakeTimeProvider));
+        ex.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void UploadFile_Throws_Exception_With_Null_Persistor()
+    {
+        var encryptor = new Mock<IFileEncryptor>(MockBehavior.Strict);
+        var compressor = new Mock<IFileCompressor>(MockBehavior.Strict);
+        IFilePersistor persistor = null;
+        var fakeTimeProvider = new FakeTimeProvider();
+        fakeTimeProvider.SetUtcNow(new DateTimeOffset(2025, 1, 1, 0, 0, 0, TimeSpan.Zero));
+        var ex = Record.Exception(() =>
+            new UploadFileManager(persistor, encryptor.Object, compressor.Object, fakeTimeProvider));
+        ex.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
+    [Fact]
+    public void UploadFile_Throws_Exception_With_Null_Encryptor()
+    {
+        var compressor = new Mock<IFileCompressor>(MockBehavior.Strict);
+        var persistor = new Mock<IFilePersistor>(MockBehavior.Strict);
+        IFileEncryptor encryptor = null;
+        var fakeTimeProvider = new FakeTimeProvider();
+        fakeTimeProvider.SetUtcNow(new DateTimeOffset(2025, 1, 1, 0, 0, 0, TimeSpan.Zero));
+        var ex = Record.Exception(() =>
+            new UploadFileManager(persistor.Object, encryptor, compressor.Object, fakeTimeProvider));
+        ex.Should().NotBeNull().And.BeOfType<ArgumentNullException>();
+    }
+
     [Theory]
     [InlineData(null)]
     [InlineData("")]
@@ -16,9 +55,9 @@ public class UploadFileManagerTests
     [InlineData("/Name")]
     public async Task UploadFile_Throws_Exceptions_For_Invalid_FileName(string? fileName)
     {
-        var compressor = new Mock<IFileCompressor>();
-        var encryptor = new Mock<IFileEncryptor>();
-        var persistor = new Mock<IFilePersistor>();
+        var compressor = new Mock<IFileCompressor>(MockBehavior.Strict);
+        var encryptor = new Mock<IFileEncryptor>(MockBehavior.Strict);
+        var persistor = new Mock<IFilePersistor>(MockBehavior.Strict);
         var fakeTimeProvider = new FakeTimeProvider();
         fakeTimeProvider.SetUtcNow(new DateTimeOffset(2025, 1, 1, 0, 0, 0, TimeSpan.Zero));
         var sut = new UploadFileManager(persistor.Object, encryptor.Object, compressor.Object, fakeTimeProvider);
