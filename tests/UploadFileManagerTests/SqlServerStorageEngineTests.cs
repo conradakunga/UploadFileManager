f46940366f55a8ff8464b05fdd2aba13e03d634b
Conrad Akunga <conradakunga@gmail.com>
2025-04-27 22:53:54 +0300

    Implement SQL Server storage engine

---
+++ tests/UploadFileManagerTests/SqlServerStorageEngineTests.cs
@@ -0,0 +1,132 @@
+using System.Security.Cryptography;
+using System.Text;
+using Bogus;
+using FluentAssertions;
+using Microsoft.Extensions.Time.Testing;
+using Rad.UploadFileManager;
+
+namespace UploadFileManagerTests;
+
+public class SqlServerStorageEngineTests
+{
+    private readonly UploadFileManagerBehaviour _managerBehaviour;
+
+    public SqlServerStorageEngineTests()
+    {
+        // Create a file compressor
+        var compressor = new GZipCompressor();
+        //
+        // Create an encryptor
+        //
+
+        // Create Aes object
+        var aes = Aes.Create();
+        // Create the encryptor
+        var encryptor = new AesFileEncryptor(aes.Key, aes.IV);
+
+        // Create the storage engine
+        var storageEngine =
+            new SqlServerStorageEngine("data source=localhost;uid=sa;pwd=YourStrongPassword123;Database=FileStore;TrustServerCertificate=True;");
+
+        // Create the time provider
+        var timeProvider = new FakeTimeProvider();
+        timeProvider.SetUtcNow(new DateTimeOffset(2025, 1, 1, 0, 0, 0, TimeSpan.Zero));
+
+        // Create the file manager
+        _managerBehaviour = new UploadFileManagerBehaviour(storageEngine, encryptor, compressor, timeProvider);
+    }
+
+    private static MemoryStream GetFile()
+    {
+        var faker = new Faker();
+        var dataToStore = faker.Lorem.Sentences(20);
+        var dataToStoreStream = new MemoryStream(Encoding.UTF8.GetBytes(dataToStore));
+        return dataToStoreStream;
+    }
+
+    private async Task<FileMetadata> Upload(MemoryStream data)
+    {
+        return await _managerBehaviour.UploadFileAsync("Test.txt", ".txt", data, CancellationToken.None);
+    }
+
+    [Fact]
+    public async Task Upload_And_Download_Succeeds()
+    {
+        // Get the data
+        var data = GetFile();
+        // Upload a file
+        var uploadMetadata = await Upload(data);
+        // Check the metadata
+        uploadMetadata.Should().NotBeNull();
+        uploadMetadata.FileId.Should().NotBeEmpty();
+        // Download the file
+        var download = await _managerBehaviour.DownloadFileAsync(uploadMetadata.FileId);
+        download.GetBytes().Should().BeEquivalentTo(data.GetBytes());
+    }
+
+    [Fact]
+    public async Task File_Exists_Fails_If_ID_Doesnt_Exist()
+    {
+        // Check if the file exists
+        var result = await _managerBehaviour.FileExistsAsync(Guid.Empty);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task File_Exists_Fails_If_ID_Exists()
+    {
+        // Get the data
+        var data = GetFile();
+        // Upload a file
+        var uploadMetadata = await Upload(data);
+        // Check if the file exists by ID
+        var result = await _managerBehaviour.FileExistsAsync(uploadMetadata.FileId);
+        result.Should().BeTrue();
+    }
+
+    [Fact]
+    public async Task File_Delete_Succeeds()
+    {
+        // Get the data
+        var data = GetFile();
+        // Upload a file
+        var uploadMetadata = await Upload(data);
+        // Check if the file exists
+        var result = await _managerBehaviour.FileExistsAsync(uploadMetadata.FileId);
+        result.Should().BeTrue();
+        // Delete the file
+        await _managerBehaviour.DeleteFileAsync(uploadMetadata.FileId);
+        // Check again if the file exists
+        result = await _managerBehaviour.FileExistsAsync(uploadMetadata.FileId);
+        result.Should().BeFalse();
+    }
+
+    [Fact]
+    public async Task File_GetMetadata_Succeeds()
+    {
+        // Get the data
+        var data = GetFile();
+        // Upload a file
+        var uploadMetadata = await Upload(data);
+        // Get the metadata from the ID
+        var storedMetadata = await _managerBehaviour.FetchMetadataAsync(uploadMetadata.FileId);
+        storedMetadata.Should().NotBeNull();
+        storedMetadata.Should().BeEquivalentTo(uploadMetadata);
+    }
+
+    [Fact]
+    public async Task File_GetMetadata_Fails_If_ID_Doesnt_Exist()
+    {
+        // Fetch metadata for non-existent ID
+        var ex = await Record.ExceptionAsync(() => _managerBehaviour.FetchMetadataAsync(Guid.Empty));
+        ex.Should().BeOfType<FileNotFoundException>();
+    }
+
+    [Fact]
+    public async Task File_Delete_Fails_If_ID_Doesnt_Exist()
+    {
+        // Delete a non-existent file id
+        var ex = await Record.ExceptionAsync(() => _managerBehaviour.DeleteFileAsync(Guid.Empty));
+        ex.Should().BeOfType<FileNotFoundException>();
+    }
+}
